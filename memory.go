068bf14f7635e7bd167c23bf1bb31c26491fc650
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-03-14 22:02:27 -0700

    MemorySpace is now an interface type
    - This makes it easier to generate subspaces and crazy memory layouts

--- memory.go
+++ memory.go
@@ -20,18 +20,52 @@
 // declaration of the iris2 memory layout
 package iris2
 
+import (
+	"fmt"
+)
+
 // Memory is laid out as a series of spaces (which are slices). This makes it really easy to simulate NUMA designs where we have multiple discontiguous memory spaces
 // The actual design of memory itself is abstracted through the use of the memory spaces and is up to the memory controller to actually manage things like address translation
 // The simplest design will have a single memory space
-type MemorySpace []byte
+type MemorySpace interface {
+	MemorySize() Word
+	PointerAt(address Word) (*Pointer, error)
+	BoundedPointerAt(address, size Word) (*Pointer, error)
+	Subspace(start Word) (MemorySpace, error)
+	BoundedSubspace(start, end Word) (MemorySpace, error)
+}
+
+type SimpleMemorySpace []byte
 
 // Returns slice for a given address within this space. The external address is not the spaceAddress in cases with multiple memory spaces
-func (this MemorySpace) RawPointer(spaceAddress Word) []byte {
-	return this[spaceAddress:]
+func (this SimpleMemorySpace) BoundedSubspace(start, end Word) (MemorySpace, error) {
+	return SimpleMemorySpace(this[start:end]), nil
 }
-func (this MemorySpace) BoundedRawPointer(spaceAddressBegin, spaceAddressEnd Word) []byte {
-	return this[spaceAddressBegin:spaceAddressEnd]
+func (this SimpleMemorySpace) Subspace(start Word) (MemorySpace, error) {
+	return SimpleMemorySpace(this[start:]), nil
 }
 
-type MemoryController interface {
+func (this SimpleMemorySpace) MemorySize() Word {
+	return Word(len(this))
+}
+
+func (this SimpleMemorySpace) PointerAt(address Word) (*Pointer, error) {
+	if address >= Word(len(this)) {
+		return nil, fmt.Errorf("Provided address %d is out of range!", address)
+	} else {
+		return NewPointer(this[address:], address), nil
+	}
+}
+
+func (this SimpleMemorySpace) BoundedPointerAt(address, size Word) (*Pointer, error) {
+	if address >= Word(len(this)) {
+		return nil, fmt.Errorf("Provided address %d is out of range!", address)
+	}
+
+	// hmmm, I have to think if there are other kinds of failure states
+	if size > Word(len(this)) || (size > (Word(len(this)) - address)) {
+		return nil, fmt.Errorf("Provided bounding size %d is too big!", size)
+	}
+
+	return NewPointer(this[address:(address+size)], address), nil
 }
