fbe8a86382191b0eb5299749f39b4ded731b9429
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-03-22 12:47:02 -0700

    Added model.go
    - This file will contain different core designs and is designed to get me out
    of design hell.

---
+++ model.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2015 Joshua Scoggins
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgement in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+//
+
+// iris core designs
+package iris2
+
+type FirstGenCore struct {
+	Registers   [256]Word
+	OnDieMemory SimpleMemorySpace // memory + pointers
+	Code, Data  MemorySpace
+}
+
+func megabyte(count Word) Word {
+	return 1024 * kilobyte(count)
+}
+func kilobyte(count Word) Word {
+	return 1024 * count
+}
+func NewFirstGenCore(byteCount Word) (*FirstGenCore, error) {
+	var core FirstGenCore
+	for i := 0; i < 256; i++ {
+		core.Registers[i] = 0
+	}
+	core.OnDieMemory = make(SimpleMemorySpace, byteCount)
+	ptr0, err0 := core.OnDieMemory.BoundedSubspace(0, byteCount/2)
+	if err0 != nil {
+		return nil, err0
+	} else {
+		core.Code = ptr0
+	}
+	ptr1, err1 := core.OnDieMemory.Subspace(byteCount / 2)
+	if err1 != nil {
+		return nil, err1
+	} else {
+		core.Data = ptr1
+	}
+	return &core, nil
+}
